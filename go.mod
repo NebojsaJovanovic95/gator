a728c33b38b1f7db6b256af4257b4dad7802f226
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-02-16 15:06:15 -0500

    sqlc and goose setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NebojsaJovanovic95/gator.git
 
 go 1.25.5
+
+require github.com/lib/pq v1.11.2 // indirect
---
+++ sql/schema/001_users.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+--    email TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+);
+
+-- +goose Down
+DROP TABLE users;
